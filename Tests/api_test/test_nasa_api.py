b83d365cedcfc65277f1cc7f65a57f1bdbd300ae
student <yra.shubin@gmail.com>
2026-03-05 21:15:28 +0500

    git

--- Tests/api_test/test_nasa_api.py
+++ Tests/api_test/test_nasa_api.py
@@ -14,10 +14,25 @@ a = 2
 # @pytest.mark.skipif(a = 2, reason='Пропустить тест если переменная a = 2')
 # @pytest.mark.skip(reason="Метод получение экспериментов не работает.")
 # @pytest.mark.parametrize("language", ["eng", "uz", "kz"])
+
+
 def test_get_experiment():
     url = "https://osdr.nasa.gov/geode-py/ws/api/vehicles"
-    with allure.step("Step 1"):
-        resp = requests.get(url).json()
+    Vehicle = {
+        "id": 4,
+        "name": "Discovery",
+        "status": "available"
+    }
+
+
+    response = requests.get("https://osdr.nasa.gov/geode-py/ws/api/vehicle/Discovery")
+    print (response.status_code)  # 200
+    print (response.json())#["name"] == "Discovery"
+    try:
+        response = requests.get("https://osdr.nasa.gov/geode-py/ws/api/vehicle/Discovery", timeout=5)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print("Ошибка запроса:", e)
 
     with allure.step("Step 1"):
         pass
